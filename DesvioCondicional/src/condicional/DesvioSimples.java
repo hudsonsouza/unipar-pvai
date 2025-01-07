f22f0eed3d4ed93e88c14a579b0a312671dd6b99
Hudson Souza <hudsonss@gmail.com>
2025-01-07 18:03:42 -0300

    Arquivo: condicional/DesvioSimples.java

---
+++ DesvioCondicional/src/condicional/DesvioSimples.java
@@ -0,0 +1,11 @@
+package condicional;
+public class DesvioSimples {
+    public static void main(String[] args) {
+
+        int idade = 6;
+
+        if (idade < 18) {
+            System.out.println("Usuário menor de idade com " + idade + " anos");
+        }
+    }
+}
