5c2099373223a0e9ebc626fc8983c71c5c6504c2
Hudson Souza <hudsonss@gmail.com>
2025-01-08 19:48:29 -0300

    listaExerc01/L1Ex05.java

---
+++ DesvioCondicional/src/listaExerc01/L1Ex05.java
@@ -0,0 +1,32 @@
+package listaExerc01;
+
+import java.util.Scanner;
+
+public class L1Ex05 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String senha = "Unipar2025", senhaUsuario;
+        boolean autenticacao = false;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Digite sua senha...: ");
+            senhaUsuario = input.nextLine();
+
+            if (senha.equals(senhaUsuario)) {
+                System.out.println("ACESSO PERMITIDO!");
+                autenticacao = true;
+                break;
+            } else {
+                System.out.println("ACESSO NEGADO.");
+                autenticacao = false;
+            }
+        }
+
+        if (autenticacao == false) {
+            System.out.println("Limite de 3 tentativas esgotadas.");
+            System.out.println("### CARTÃO BLOQUEADO ###");
+        }
+
+    }
+}
