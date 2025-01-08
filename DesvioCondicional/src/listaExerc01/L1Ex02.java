a2d5f659d66e8e2a859707a8f8968c43c6b14e05
Hudson Souza <hudsonss@gmail.com>
2025-01-08 18:55:38 -0300

    listaExerc01/L1Ex02.java

---
+++ DesvioCondicional/src/listaExerc01/L1Ex02.java
@@ -0,0 +1,15 @@
+package listaExerc01;
+public class L1Ex02 {
+    public static void main(String[] args) {
+
+        for(int i=0; i<10; i++){
+
+            if(i%2==0){
+                System.out.println(i+" - PAR");
+            } else {
+                System.out.println(i+" - IMPAR");
+            }
+
+        }
+    }
+}
